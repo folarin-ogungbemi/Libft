fdf384af96653f045fdcb13781f84ebfe5781f5f
folarin-ogungbemi <ftogungbemi@gmail.com>
2024-11-11 13:29:52 +0100

    add comments to code

--- srcs/ft_isalpha.c
+++ srcs/ft_isalpha.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <ctype.h>
 
+/* Function checks for an alphabetic char in the standard C locale */
+
 int	ft_isalpha(char c)
 {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
@@ -12,11 +14,13 @@ int	ft_isalpha(char c)
 
 int	main(void)
 {
-	if (isalpha('A'))
+	// Library Test
+	if (isalpha('z'))
 		write(1, "Library: True\n", 14);
 	else
 		write(1, "Library: False\n", 15);
-	if (ft_isalpha('A'))
+	// Custom code Test
+	if (ft_isalpha('z'))
 		write(1, "Custom: True\n", 13);
 	else
 		write(1, "Custom: False\n", 14);
