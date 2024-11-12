9e51be1e8fc83b22dbfd61248f035a450ea9184a
folarin-ogungbemi <ftogungbemi@gmail.com>
2024-11-12 11:04:09 +0100

    Add test case to ft_isalpha

--- srcs/ft_isalpha.c
+++ srcs/ft_isalpha.c
@@ -1,9 +1,10 @@
 
 #include <unistd.h>
 #include <ctype.h>
+#include <check.h>
 
-/* Function checks for an alphabetic char in the standard C locale */
-
+/* Function checks for alphabetic character
+ * in the standard C locale */
 int	ft_isalpha(char c)
 {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
@@ -12,17 +13,42 @@ int	ft_isalpha(char c)
 		return (0);
 }
 
+/* Define a test case for for ft_isalpha */
+START_TEST(test_ft_isalpha)
+{
+	int	c;
+
+	c = 0;
+	while (c <= 127)
+	{
+		ck_assert_int_eq((ft_isalpha(c) != 0), (isalpha(c) != 0));
+		c++;
+	}
+}
+END_TEST
+
+/* Define a test suite for ft_isalpha */
+Suite *ft_isalpha_suite(void)
+{
+	Suite *s = suite_create("ft_isalpha Suite");
+	TCase *tc_core = tcase_create("Core");
+
+	tcase_add_test(tc_core, test_ft_isalpha);
+	suite_add_tcase(s, tc_core);
+
+	return s;
+}
+
 int	main(void)
 {
-	// Library Test
-	if (isalpha('z'))
-		write(1, "Library: True\n", 14);
-	else
-		write(1, "Library: False\n", 15);
-	// Custom code Test
-	if (ft_isalpha('z'))
-		write(1, "Custom: True\n", 13);
-	else
-		write(1, "Custom: False\n", 14);
-	return (0);
+	int	number_failed;
+
+	Suite *s = ft_isalpha_suite();
+	SRunner *sr = srunner_create(s);
+
+	srunner_run_all(sr, CK_NORMAL);
+	number_failed = srunner_ntests_failed(sr);
+	srunner_free(sr);
+
+	return (number_failed == 0) ? 0 : 1;
 }
