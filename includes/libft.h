e7a3a0659aa8707510a9d22366fd98062dbe6885
folarin-ogungbemi <ftogungbemi@gmail.com>
2024-11-15 19:43:07 +0100

    create header file libft.h

---
+++ includes/libft.h
@@ -0,0 +1,12 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdio.h>
+# include <stddef.h>
+# include <ctype.h>
+
+int	ft_isalpha(char c);
+int	ft_isalnum(char c);
+int	ft_isdigit(char c);
+void    *ft_memmove(void *dest, const void *src, size_t n);
+#endif
