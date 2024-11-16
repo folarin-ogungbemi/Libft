225b01214960032cdf455c73a41bca0d48097f55
folarin-ogungbemi <ftogungbemi@gmail.com>
2024-11-16 16:37:00 +0100

    create func: replica strlcpy libc

---
+++ srcs/ft_strlcpy.c
@@ -0,0 +1,32 @@
+#include <check.h>
+#include <stdio.h>
+#include <string.h>
+#include "../includes/libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	int	i;
+
+	if (size > 0)
+	{
+		i = 0;
+		while (i < (size - 1) && src[i])
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (ft_strlen(src));
+}
+/*
+int	main(void)
+{
+	char	buffer[5];
+	const char *src = "Hello, World!";
+
+	printf("result: %ld\n", ft_strlcpy(buffer, src, sizeof(buffer)));
+	printf("buffer: %s\n", buffer);
+	return (0);
+}
+*/
