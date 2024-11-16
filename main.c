0dd325cf457117cb04937323ca72c919ed0b5e24
folarin-ogungbemi <ftogungbemi@gmail.com>
2024-11-16 23:01:08 +0100

    seperate production program

--- main.c
+++ main.c
@@ -1,15 +1,12 @@
+#include "./includes/libft.h"
 
-#include "libft.h"
-
-int	main(void)
-{
-	//unsigned char	dest[5];
-	char	src[] = "ABCDEFGHI";
-
-	printf("before: %s\n", src);
-
-	//memmove(src + 3, src, 5);
-	ft_memmove(src + 3, src, 5);
-	printf("after: %s\n", src);
-	return (0);
+void production_function() {
+    printf("Production function executed.\n");
 }
+
+int     main(void)
+{
+	printf("Running production...\n");
+	production_function();
+	return (0);
+} 
