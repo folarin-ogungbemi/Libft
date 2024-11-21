ef6c837840178b1acabee74619018009e1830189
folarin-ogungbemi <ftogungbemi@gmail.com>
2024-11-21 02:17:53 +0100

    Create strlcat function and test to pass

--- includes/libft.h
+++ includes/libft.h
@@ -5,6 +5,7 @@
 # include <stddef.h> // func lib
 # include <ctype.h> // func lib
 # include <check.h> // Test lib
+# include <string.h> // func strings
 
 # define TEXT 				"\033[0;90m" // grey
 # define WARNING			"\033[0;33m" // yellow
@@ -19,6 +20,7 @@ int	ft_isdigit(char c);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 void    *ft_memmove(void *dest, const void *src, size_t n);
+size_t  ft_strlcat(char *dst, const char *src, size_t size);
 
 /* Test files */
 int run_test_ft_isalpha(void);
@@ -27,4 +29,5 @@ int run_test_ft_isdigit(void);
 int run_test_ft_memmove(void);
 int run_test_ft_strlen(void);
 int run_test_ft_strlcpy(void);
+int run_test_ft_strlcat(void);
 #endif
