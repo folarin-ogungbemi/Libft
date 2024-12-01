c0e53831299bace887f42d847d5a6357c2e70f7a
folarin-ogungbemi <ftogungbemi@gmail.com>
2024-12-02 00:07:16 +0100

    Include library to file

--- ft_bzero.c
+++ ft_bzero.c
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stddef.h>
+
 /* The  bzero()  function  erases  the data in the n bytes
  * of the memory starting at the location pointed to by s,
  * by writing zeros (bytes containing '\0') to that area.*/
